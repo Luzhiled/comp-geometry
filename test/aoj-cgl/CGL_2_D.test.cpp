800563c305f35072e9de377859de1624b8d19701
Luzhiled <Luzhiled+github@gmail.com>
2020-10-09 01:14:55 +0900

    add test distance

---
+++ test/aoj-cgl/CGL_2_D.test.cpp
@@ -0,0 +1,23 @@
+// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_D
+// verification-helper: ERROR 0.00000001
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../src/distance.cpp"
+#undef call_from_test
+
+using namespace geometry;
+int main() {
+  int q;
+  cin >> q;
+
+  cout << fixed << setprecision(15);
+  while (q--) {
+    segment s1, s2;
+    cin >> s1.a >> s1.b >> s2.a >> s2.b;
+
+    cout << distance(s1, s2) << endl;
+  }
+}
