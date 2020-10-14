d8f70587477fa378380973dafc85b0dcd279fc9b
Luzhiled <Luzhiled+github@gmail.com>
2020-10-14 17:36:32 +0900

    add test Convex Cut

---
+++ test/aoj-cgl/CGL_4_C.test.cpp
@@ -0,0 +1,33 @@
+// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/4/CGL_4_C
+// verification-helper: ERROR 0.00001
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "../../src/line.hpp"
+#include "../../src/area.hpp"
+#include "../../src/convex_cut.hpp"
+#undef call_from_test
+
+using namespace geometry;
+int main() {
+  int n;
+  cin >> n;
+
+  polygon poly(n);
+  for (auto &p: poly) {
+    cin >> p;
+  }
+
+  int q;
+  cin >> q;
+
+  cout << fixed << setprecision(15);
+  while (q--) {
+    line l;
+    cin >> l.a >> l.b;
+
+    cout << area(convex_cut(poly, l)) << endl;
+  }
+}
