a3c4aaa507b1505b3e249715d17e9ae75727af9e
Luzhiled <Luzhiled+github@gmail.com>
2020-10-27 06:59:11 +0900

    fix

--- src/convex_diameter.hpp
+++ src/convex_diameter.hpp
@@ -9,22 +9,21 @@ using namespace std;
 #include "./compare.hpp"
 
 namespace geometry {
-  real_number convex_diameter(const polygon &poly) {
-    int n = poly.size();
-    if (n == 2) return abs(poly[0] - poly[1]);
+  real_number convex_diameter(const polygon &p) {
+    int n = p.size();
+    if (n == 2) return abs(p[0] - p[1]);
 
-    polygon p = poly;
     int i = 0, j = 0;
     for (int k = 0; k < n; ++k) {
-      if (compare_x(poly[i], poly[k])) i = k;
-      if (!compare_x(poly[j], poly[k])) j = k;
+      if ( compare_x(p[i], p[k])) i = k;
+      if (!compare_x(p[j], p[k])) j = k;
     }
 
     real_number res = 0;
     int si = i, sj = j;
     while (i != sj || j != si) {
-      res = max(res, abs(poly[i] - poly[j]));
-      if (sign(cross(poly[(i + 1) % n] - p[i], p[(j + 1) % n] - p[j])) == -1) {
+      res = max(res, abs(p[i] - p[j]));
+      if (sign(cross(p[(i + 1) % n] - p[i], p[(j + 1) % n] - p[j])) == -1) {
         i = (i + 1) % n;
       } else {
         j = (j + 1) % n;
