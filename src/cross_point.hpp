2d4fbc7acd40c7bff8002edd03cfbd3c4c6ce511
Luzhiled <Luzhiled+github@gmail.com>
2020-10-27 07:41:31 +0900

    fix

--- src/cross_point.hpp
+++ src/cross_point.hpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 #include "./base.hpp"
-#include "./segment.hpp"
+#include "./line.hpp"
 #include "./product.hpp"
 
 // cross point
 namespace geometry {
-  point cross_point(const segment &s1, const segment &s2) {
+  point cross_point(const line &s1, const line &s2) {
     real_number a = cross(s1.b - s1.a, s2.b - s2.a);
     real_number b = cross(s1.b - s1.a, s1.b - s2.a);
     if (is_equal(a, 0) && is_equal(b, 0)) return s2.a;
