137e25b9e2fdab79cc098bd3da47c938e19e3be5
Luzhiled <Luzhiled+github@gmail.com>
2020-10-27 12:22:03 +0900

    add common tangent

---
+++ test/aoj-cgl/CGL_7_G.test.cpp
@@ -0,0 +1,24 @@
+// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/CGL_7_G
+// verification-helper: ERROR 0.00001
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../../src/tangent_cc.hpp"
+#include "../../src/projection.hpp"
+#include "../../src/compare.hpp"
+
+using namespace geometry;
+int main() {
+  circle c1, c2;
+  cin >> c1.p >> c1.r;
+  cin >> c2.p >> c2.r;
+
+  lines ls = tangent_cc(c1, c2);
+  points ps;
+  for (auto &l : ls) ps.emplace_back(projection(l, c1.p));
+  sort(ps.begin(), ps.end(), compare_x);
+
+  cout << fixed << setprecision(20);
+  for (auto &p : ps) cout << p << endl;
+}
