481d8dcc453b50b9ae79c40f7524ba72a62b44d0
Luzhiled <Luzhiled+github@gmail.com>
2020-10-27 11:45:12 +0900

    fix #4

--- src/convex_cut.hpp
+++ src/convex_cut.hpp
@@ -16,11 +16,11 @@ namespace geometry {
     for (int i = 0; i < n; ++i) {
       int j = (i + 1 == n ? 0 : i + 1);
 
-      real_number ci = cross(l.a - poly[i], l.b - poly[i]);
-      real_number cj = cross(l.a - poly[j], l.b - poly[j]);
+      real_number cf = cross(l.a - poly[i], l.b - poly[i]);
+      real_number cs = cross(l.a - poly[j], l.b - poly[j]);
 
-      if (sign(ci) >= 0) res.emplace_back(poly[i]);
-      if (sign(ci) * sign(cj) < 0) {
+      if (sign(cf) >= 0) res.emplace_back(poly[i]);
+      if (sign(cf) * sign(cs) < 0) {
         real_number s = cross(poly[j] - poly[i], l.a - l.b);
         real_number t = cross(l.a - poly[i], l.a - l.b);
         res.emplace_back(poly[i] + t / s * (poly[j] - poly[i]));
