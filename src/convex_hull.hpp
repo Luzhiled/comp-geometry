68e6a91bd6e12d09eceb9c9fa44d51c359700f21
Luzhiled <Luzhiled+github@gmail.com>
2020-10-27 20:17:19 +0900

    fix #2

--- src/convex_hull.hpp
+++ src/convex_hull.hpp
@@ -13,19 +13,20 @@ namespace geometry {
   polygon convex_hull(points poly) {
     int n = poly.size(), k = 0;
     if (n <= 2) return poly;
-    sort(poly.begin(), poly.end(), compare_x);
 
+    sort(poly.begin(), poly.end(), compare_x);
     polygon res(2 * n);
+
+    auto check = [&](int i) {
+      return sign(cross(res[k - 1] - res[k - 2], poly[i] - res[k - 1])) == -1;
+    };
+
     for (int i = 0; i < n; res[k++] = poly[i++]) {
-      while (k >= 2 && sign(cross(res[k - 1] - res[k - 2], poly[i] - res[k - 1])) == -1) {
-        --k;
-      }
+      while (k >= 2 and check(i)) --k;
     }
 
     for (int i = n - 2, t = k + 1; i >= 0; res[k++] = poly[i--]) {
-      while (k >= t && sign(cross(res[k - 1] - res[k - 2], poly[i] - res[k - 1])) == -1) {
-        --k;
-      }
+      while (k >= t and check(i)) --k;
     }
 
     res.resize(k - 1);
