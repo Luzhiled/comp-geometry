0452fa060063fe5cc5721ecde79b55f9c88a8e79
Luzhiled <Luzhiled+github@gmail.com>
2020-11-18 11:52:21 +0900

    add test aoj 2442

---
+++ test/aoj/icpc/2442.test.cpp
@@ -0,0 +1,47 @@
+// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/2442
+// verification-helper: ERROR 0.0001
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+#include "../../../src/point.hpp"
+#include "../../../src/segment.hpp"
+#include "../../../src/is_parallel.hpp"
+
+using namespace geometry;
+
+void drop() {
+  cout << "NA" << endl;
+  exit(0);
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  if (n & 1) drop();
+
+  points ps(n);
+  for (auto &p : ps) {
+    cin >> p;
+  }
+
+  segments ss;
+  for (int i = 0; i < n; i++) {
+    ss.emplace_back(ps[i], ps[(i + 1) % n]);
+  }
+
+  for (int i = 0; i < n; i++) {
+    int j = (i + n / 2) % n;
+    if (is_parallel(ss[i], ss[j]) == false) {
+      drop();
+    }
+
+    if (is_equal(abs(ss[i].a - ss[i].b), abs(ss[j].a - ss[j].b)) == false) {
+      drop();
+    }
+  }
+
+  cout << (ps[0] + ps[(n / 2) % n]) * (1.0 / 2.0) << endl;
+}
