72a1b8bd1b26c9435c71ad64b5cb8367138fa242
Luzhiled <Luzhiled+github@gmail.com>
2021-01-16 18:41:44 +0900

    fix

--- src/circumscribed_circle.hpp
+++ src/circumscribed_circle.hpp
@@ -4,15 +4,13 @@
 #include "./circle.hpp"
 #include "./point.hpp"
 #include "./line.hpp"
-#include "./cross_point_ll.hpp"
+#include "./product.hpp"
 
 namespace geometry {
   circle circumscribed_circle(const point &a, const point &b, const point &c) {
-    point m1((a + b) / real_number(2)), m2((b + c) / real_number(2));
-    point v((b - a).imag(), (a - b).real()), w((b - c).imag(), (c - b).real());
-    line s(m1, point(m1 + v)), t(m2, point(m2 + w));
-
-    point p = cross_point_ll(s, t);
-    return circle(p, abs(a - p));
+    real_number A = norm(b - c), B = norm(c - a), C = norm(a - b);
+    real_number S = norm(cross(b - a, c - a)), T = A + B + C;
+    point p = (A*(T - 2*A) * a + B*(T - 2*B) * b + C*(T - 2*C) * c) / (4 * S);
+    return circle(p, abs(p - a));
   }
 }
