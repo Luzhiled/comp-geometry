0095d8af29062d470a84a4e9087071283e1a83d4
Luzhiled <Luzhiled+github@gmail.com>
2021-03-31 06:55:23 +0900

    fix header

--- src/angle.hpp
+++ src/angle.hpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 
 #include "./base.hpp"
+#include "./point.hpp"
 
 // angle
 namespace geometry {
@@ -13,6 +14,17 @@ namespace geometry {
 
   real_number degree_to_radian(const real_number deg) {
     return deg * PI / 180.0;
+  }
+
+  real_number get_angle(const point &a, const point &b, const point &c) {
+    const point u(a - b), v(c - b);
+    auto fix = [](real_number theta) {
+      if (theta < 0) theta += 2 * PI;
+      return theta;
+    };
+    real_number alpha = fix(atan2(u.imag(), u.real()));
+    real_number beta  = fix(atan2(v.imag(), v.real()));
+    return fix(beta - alpha);
   }
 
   real_number get_smaller_angle(const point &a, const point &b, const point &c) {
