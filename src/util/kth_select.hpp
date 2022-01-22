a21102d6cddc678cb26828c2e536138947a91424
Luzhiled <Luzhiled+github@gmail.com>
2022-01-23 04:08:28 +0900

    new kth_select.cpp

---
+++ src/util/kth_select.hpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include <cstddef>
+
+#include "impl_kth_select.hpp"
+
+template< typename T >
+T kth_select(std::vector< T > vs, std::size_t k) {
+  return impl_kth_select<T>(std::begin(vs), std::end(vs), k, std::less< T >());
+}
+
+template< typename T, class Compare >
+T kth_select(std::vector< T > vs, std::size_t k, Compare comp) {
+  return impl_kth_select<T>(std::begin(vs), std::end(vs), k, comp);
+}
