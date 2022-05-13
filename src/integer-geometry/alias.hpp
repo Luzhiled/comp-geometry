53528e026a73ea61937ce86cc23130e424d7fffd
Luzhiled <Luzhiled+github@gmail.com>
2022-05-14 01:00:16 +0900

    add integer-geometry/alias

---
+++ src/integer-geometry/alias.hpp
@@ -0,0 +1,12 @@
+#include <cstddef>
+#include <cstdint>
+
+namespace intgeometry2d {
+  using isize = std::ptrdiff_t;
+  using usize = std::size_t;
+
+  using i32 = std::int_fast32_t;
+  using i64 = std::int_fast64_t;
+  using u32 = std::uint_fast32_t;
+  using u64 = std::uint_fast64_t;
+} // intgeometry2d
