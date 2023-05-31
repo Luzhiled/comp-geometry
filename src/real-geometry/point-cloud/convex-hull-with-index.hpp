1b465fc608a05270e1c63599ad8b835bbf723939
Luzhiled <Luzhiled+github@gmail.com>
2023-05-31 11:43:37 +0900

    split convex-hull.hpp

---
+++ src/real-geometry/point-cloud/convex-hull-with-index.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "src/real-geometry/common/size-alias.hpp"
+#include "src/real-geometry/class/point.hpp"
+#include "src/real-geometry/class/polygon.hpp"
+#include "src/real-geometry/compare/compare-x.hpp"
+#include "src/real-geometry/operation/cross-product.hpp"
+
+#include <algorithm>
+#include <numeric>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+namespace geometry {
+
+  template< typename R >
+  std::pair< polygon<R>, std::vector< usize > > convex_hull_with_index(const points<R> &pts) {
+    usize n = pts.size();
+    if (n <= 2) {
+      std::vector< usize > idxs(n);
+      std::iota(idxs.begin(), idxs.end(), 0);
+      return {pts, idxs};
+    }
+
+    std::vector< std::pair< point<R>, usize > > ps;
+    ps.reserve(n);
+    for (usize i = 0; i < n; i++) {
+      ps.emplace_back(pts[i], i);
+    }
+
+    auto cmp = [](const std::pair<point<R>, usize> &a, const std::pair<point<R>, usize> &b) {
+      return compare_x(a.first, b.first);
+    };
+    std::sort(ps.begin(), ps.end(), cmp);
+
+    std::vector< usize > idxs(2 * n);
+    polygon<R> poly(2 * n);
+    isize k = 0, i = 0;
+
+    auto check = [&](usize i) {
+      return sign(cross_product(poly[k - 1] - poly[k - 2], ps[i].first - poly[k - 1])) == -1;
+    };
+
+    while (i < n) {
+      while (k >= 2 and check(i)) k--;
+
+      std::tie(poly[k], idxs[k]) = ps[i];
+      k++; i++;
+    }
+
+    i = n - 2;
+    usize t = k + 1;
+    while (i >= 0) {
+      while (k >= t and check(i)) k--;
+
+      std::tie(poly[k], idxs[k]) = ps[i];
+      k++; i--;
+    }
+
+    poly.resize(k - 1);
+    idxs.resize(k - 1);
+    return {poly, idxs};
+  }
+
+}
--- src/real-geometry/point-cloud/convex-hull.hpp
+++ src/real-geometry/point-cloud/convex-hull.hpp
@@ -1,86 +1,38 @@
 #pragma once
 
-#include <tuple>
-#include <utility>
-#include <vector>
-#include <numeric>
+#include "src/real-geometry/common/size-alias.hpp"
+#include "src/real-geometry/class/point.hpp"
+#include "src/real-geometry/class/polygon.hpp"
+#include "src/real-geometry/compare/compare-x.hpp"
+#include "src/real-geometry/operation/cross-product.hpp"
+
 #include <algorithm>
 
-#include "./point.hpp"
-#include "./polygon.hpp"
-#include "./compare.hpp"
-#include "./product.hpp"
-
-// convex_hull
 namespace geometry {
-  pair< polygon, vector< int > > convex_hull_with_index(const points &pts) {
-    int n = pts.size();
-    if (n <= 2) {
-      vector< int > idxs(n);
-      iota(idxs.begin(), idxs.end(), 0);
-      return {pts, idxs};
-    }
 
-    vector< pair< point, int > > ps;
-    ps.reserve(n);
-    for (int i = 0; i < n; i++) {
-      ps.emplace_back(pts[i], i);
-    }
-
-    auto extension_compare_x = [](const pair<point, int> &a, const pair<point, int> &b) {
-      return compare_x(a.first, b.first);
-    };
-    sort(ps.begin(), ps.end(), extension_compare_x);
-
-    vector< int > idxs(2 * n);
-    polygon poly(2 * n);
-    int k = 0, i = 0;
-
-    auto check = [&](int i) {
-      return sign(cross(poly[k - 1] - poly[k - 2], ps[i].first - poly[k - 1])) == -1;
-    };
-
-    while (i < n) {
-      while (k >= 2 and check(i)) k--;
-
-      tie(poly[k], idxs[k]) = ps[i];
-      k++; i++;
-    }
-
-    i = n - 2;
-    int t = k + 1;
-    while (i >= 0) {
-      while (k >= t and check(i)) k--;
-
-      tie(poly[k], idxs[k]) = ps[i];
-      k++; i--;
-    }
-
-    poly.resize(k - 1);
-    idxs.resize(k - 1);
-    return {poly, idxs};
-  }
-
-  polygon convex_hull(points pts) {
-    int n = pts.size(), k = 0;
+  template< typename R >
+  polygon<R> convex_hull(points<R> pts) {
+    usize n = pts.size(), k = 0;
     if (n <= 2) return pts;
 
-    sort(pts.begin(), pts.end(), compare_x);
-    polygon res(2 * n);
+    std::sort(pts.begin(), pts.end(), compare_x<R>);
+    polygon<R> res(2 * n);
 
-    auto check = [&](int i) {
-      return sign(cross(res[k - 1] - res[k - 2], pts[i] - res[k - 1])) == -1;
+    auto check = [&](usize i) {
+      return sign(cross_product<R>(res[k - 1] - res[k - 2], pts[i] - res[k - 1])) == -1;
     };
 
-    for (int i = 0; i < n; res[k++] = pts[i++]) {
+    for (usize i = 0; i < n; res[k++] = pts[i++]) {
       while (k >= 2 and check(i)) --k;
     }
 
-    for (int i = n - 2, t = k + 1; i >= 0; res[k++] = pts[i--]) {
+    usize t = k + 1;
+    for (isize i = n - 2; i >= 0; res[k++] = pts[i--]) {
       while (k >= t and check(i)) --k;
     }
 
     res.resize(k - 1);
     return res;
   }
+
 }
