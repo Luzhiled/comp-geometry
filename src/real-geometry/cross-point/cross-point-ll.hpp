7df1541fa143c29fd4fe7a3eafc9a8790ffeedc1
Luzhiled <Luzhiled+github@gmail.com>
2023-05-31 10:25:01 +0900

    fix 2_C

--- src/real-geometry/cross-point/cross-point-ll.hpp
+++ src/real-geometry/cross-point/cross-point-ll.hpp
@@ -1,15 +1,17 @@
 #pragma once
 
-#include "./base.hpp"
-#include "./line.hpp"
-#include "./product.hpp"
+#include "src/real-geometry/class/line.hpp"
+#include "src/real-geometry/operation/cross-product.hpp"
+#include "src/real-geometry/utility/equals/real-number.hpp"
 
-// cross point
 namespace geometry {
-  point cross_point_ll(const line &l1, const line &l2) {
-    real_number a = cross(l1.b - l1.a, l2.b - l2.a);
-    real_number b = cross(l1.b - l1.a, l1.b - l2.a);
-    if (equals(a, 0) && equals(b, 0)) return l2.a;
+
+  template< typename R >
+  point<R> cross_point_ll(const line<R> &l1, const line<R> &l2) {
+    R a = cross_product<R>(l1.b - l1.a, l2.b - l2.a);
+    R b = cross_product<R>(l1.b - l1.a, l1.b - l2.a);
+    if (equals<R>(a, 0) && equals<R>(b, 0)) return l2.a;
     return l2.a + (l2.b - l2.a) * b / a;
   }
+
 }
