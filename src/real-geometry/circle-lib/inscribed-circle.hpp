df46b545490edf5a0be282a55501743a44b55634
Luzhiled <Luzhiled+github@gmail.com>
2023-06-01 01:49:12 +0900

    fix 7_B

--- src/real-geometry/circle-lib/inscribed-circle.hpp
+++ src/real-geometry/circle-lib/inscribed-circle.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
+#include "src/real-geometry/class/circle.hpp"
+#include "src/real-geometry/class/point.hpp"
+#include "src/real-geometry/distance/distance-sp.hpp"
+
 #include <complex>
 
-#include "./base.hpp"
-#include "./circle.hpp"
-#include "./segment.hpp"
-#include "./segment.hpp"
-#include "./distance_sp.hpp"
-
 namespace geometry {
-  circle inscribed_circle(const point &a, const point &b, const point &c) {
-    real_number A = abs(b - c), B = abs(c - a), C = abs(a - b);
-    point p((a * A + b * B + c * C) / (A + B + C));
-    real_number r = distance_sp(segment(a, b), p);
-    return circle(p, r);
+
+  template< typename R >
+  circle<R> inscribed_circle(const point<R> &a, const point<R> &b, const point<R> &c) {
+    R A = std::abs(b - c), B = std::abs(c - a), C = std::abs(a - b);
+
+    point<R> o((a * A + b * B + c * C) / (A + B + C));
+    R r = distance_sp({a, b}, o);
+
+    return {o, r};
   }
+
 }
