43a6c6720b2a3f0e4efbea6278b8011aae71b8f9
Luzhiled <Luzhiled+github@gmail.com>
2023-05-31 10:15:09 +0900

    fix header

--- test/aoj/cgl/2_B.test.cpp
+++ test/aoj/cgl/2_B.test.cpp
@@ -1,9 +1,9 @@
 // verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_B
 
-#include <iostream>
-
 #include "src/real-geometry/class/segment.hpp"
 #include "src/real-geometry/position/intersect-ss.hpp"
+
+#include <iostream>
 
 int main() {
   using R = long double;
