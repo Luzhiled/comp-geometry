56f520ba699014959ee2f2e5649a20da399b6d77
Luzhiled <Luzhiled+github@gmail.com>
2023-06-01 02:56:49 +0900

    fix 7_I

--- src/real-geometry/area/common-area-cc.hpp
+++ src/real-geometry/area/common-area-cc.hpp
@@ -1,33 +1,39 @@
 #pragma once
 
+#include "src/real-geometry/common/const/pi.hpp"
+#include "src/real-geometry/class/circle.hpp"
+#include "src/real-geometry/class/point.hpp"
+#include "src/real-geometry/utility/sign.hpp"
+
 #include <complex>
 #include <algorithm>
 #include <cmath>
 
-#include "./base.hpp"
-#include "./point.hpp"
-#include "./circle.hpp"
-
 namespace geometry {
-  real_number common_area_cc(circle a, circle b) {
-    real_number d = abs(a.p - b.p);
-    if (a.r > b.r) swap(a, b);
+
+  template< typename R >
+  R common_area_cc(circle<R> a, circle<R> b) {
+    using std::norm;
+
+    if (a.r > b.r) std::swap(a, b);
+
+    R d = std::abs(a.o - b.o);
 
     if (sign(a.r + b.r - d) <= 0) return 0;
-    if (sign(d - (b.r - a.r)) <= 0) {
-      return norm(a.r) * PI;
-    }
+    if (sign(d - (b.r - a.r)) <= 0) return norm(a.r) * pi();
 
-    real_number res = 0;
+    R res = 0;
     for (int i = 0; i < 2; ++i) {
-      real_number alpha = acos((norm(b.r) + norm(d) - norm(a.r)) / (2 * b.r * d));
-      real_number s = alpha * norm(b.r);
-      real_number t = norm(b.r) * sin(alpha) * cos(alpha);
+      R alpha = std::acos((norm(b.r) + norm(d) - norm(a.r)) / (2 * b.r * d));
+
+      R s = alpha * norm(b.r);
+      R t = norm(b.r) * std::sin(alpha) * std::cos(alpha);
       res += s - t;
-      swap(a, b);
+
+      std::swap(a, b);
     }
 
     return res;
   }
-}
 
+}
--- src/real-geometry/common/const/pi.hpp
+++ src/real-geometry/common/const/pi.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-#include "src/real-geometry/float-alias.hpp"
+#include "src/real-geometry/common/float-alias.hpp"
 
 #include <cmath>
 
 namespace geometry {
 
   static f80 pi() {
-    const f80 PI = std::acosl(-1);
+    static const f80 PI = acosl(-1); // no need `std::`. (?)
     return PI;
   }
 
