ad0c28256fb92ddfb3217ea7d80d27f8221c5ad4
Luzhiled <Luzhiled+github@gmail.com>
2023-06-01 02:20:58 +0900

    fix 7_F

--- src/real-geometry/circle-lib/tangent-cp.hpp
+++ src/real-geometry/circle-lib/tangent-cp.hpp
@@ -1,17 +1,19 @@
 #pragma once
 
+#include "src/real-geometry/class/circle.hpp"
+#include "src/real-geometry/class/point.hpp"
+#include "src/real-geometry/cross-point/cross-point-cc.hpp"
+
 #include <complex>
 #include <cmath>
 
-#include "./base.hpp"
-#include "./circle.hpp"
-#include "./line.hpp"
-#include "./point.hpp"
-#include "./cross_point_cc.hpp"
-
 namespace geometry {
-  points tangent_cp(const circle &c, const point &p) {
-    circle t(p, sqrt(norm(c.p - p) - norm(c.r)));
+
+  template< typename R >
+  points<R> tangent_cp(const circle<R> &c, const point<R> &p) {
+    circle<R> t{p, std::sqrt(std::norm(c.o - p) - std::norm(c.r))};
+
     return cross_point_cc(c, t);
   }
+
 }
--- test/aoj/cgl/7_F.test.cpp
+++ test/aoj/cgl/7_F.test.cpp
@@ -1,25 +1,26 @@
-// verification-helper: IGNORE
 // verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/CGL_7_F
 // verification-helper: ERROR 0.00001
+
+#include "src/real-geometry/circle-lib/tangent-cp.hpp"
+#include "src/real-geometry/class/circle.hpp"
+#include "src/real-geometry/class/point.hpp"
+#include "src/real-geometry/compare/compare-x.hpp"
+#include "src/real-geometry/utility/io-set.hpp"
 
 #include <iostream>
 #include <algorithm>
 
-#include "../../../src/tangent_cp.hpp"
-#include "../../../src/point.hpp"
-#include "../../../src/compare.hpp"
-#include "../../../src/util/io_set.hpp"
-
-using namespace geometry;
 int main() {
-  point p;
+  using R = long double;
+
+  geometry::point<R> p;
   std::cin >> p;
 
-  circle c;
-  std::cin >> c.p >> c.r;
+  geometry::circle<R> c;
+  std::cin >> c.o >> c.r;
 
-  points pts = tangent_cp(c, p);
-  sort(pts.begin(), pts.end(), compare_x);
+  geometry::points<R> pts = geometry::tangent_cp(c, p);
+  std::sort(pts.begin(), pts.end(), geometry::compare_x<R>);
 
-  for (auto &p : pts) std::cout << p << std::endl;
+  for (auto &p : pts) std::cout << p.x() << " " << p.y() << std::endl;
 }
