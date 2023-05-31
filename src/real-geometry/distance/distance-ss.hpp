388a0b640b5756977fe02a3e6715fce1dae5fd2c
Luzhiled <Luzhiled+github@gmail.com>
2023-05-31 10:25:11 +0900

    fix 2_D

--- src/real-geometry/distance/distance-ss.hpp
+++ src/real-geometry/distance/distance-ss.hpp
@@ -1,20 +1,21 @@
 #pragma once
 
+#include "src/real-geometry/class/segment.hpp"
+#include "src/real-geometry/distance/distance-sp.hpp"
+#include "src/real-geometry/position/intersect-ss.hpp"
+
 #include <algorithm>
 
-#include "./base.hpp"
-#include "./segment.hpp"
-#include "./is_intersect.hpp"
-#include "./distance_sp.hpp"
-
-// distance
 namespace geometry {
-  real_number distance_ss(const segment &s1, const segment &s2) {
-    if (is_intersect(s1, s2)) return 0;
-    real_number r1 = distance_sp(s1, s2.a);
-    real_number r2 = distance_sp(s1, s2.b);
-    real_number r3 = distance_sp(s2, s1.a);
-    real_number r4 = distance_sp(s2, s1.b);
-    return min({r1, r2, r3, r4});
+
+  template< typename R >
+  R distance_ss(const segment<R> &s1, const segment<R> &s2) {
+    if (intersect_ss(s1, s2)) return 0;
+    R r1 = distance_sp(s1, s2.a);
+    R r2 = distance_sp(s1, s2.b);
+    R r3 = distance_sp(s2, s1.a);
+    R r4 = distance_sp(s2, s1.b);
+    return std::min({r1, r2, r3, r4});
   }
+
 }
--- test/aoj/cgl/2_D.test.cpp
+++ test/aoj/cgl/2_D.test.cpp
@@ -1,21 +1,21 @@
-// verification-helper: IGNORE
 // verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_D
 // verification-helper: ERROR 0.00000001
 
+#include "src/real-geometry/distance/distance-ss.hpp"
+#include "src/real-geometry/utility/io-set.hpp"
+
 #include <iostream>
 
-#include "../../../src/distance_ss.hpp"
-#include "../../../src/util/io_set.hpp"
-
-using namespace geometry;
 int main() {
+  using R = long double;
+
   int q;
   std::cin >> q;
 
   while (q--) {
-    segment s1, s2;
+    geometry::segment<R> s1, s2;
     std::cin >> s1.a >> s1.b >> s2.a >> s2.b;
 
-    std::cout << distance_ss(s1, s2) << std::endl;
+    std::cout << geometry::distance_ss(s1, s2) << std::endl;
   }
 }
