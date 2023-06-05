a4bf56de885c6b0d31afceb90d0c389b2966ac50
Luzhiled <Luzhiled+github@gmail.com>
2023-06-05 10:55:13 +0900

    fix 2442

--- test/aoj/icpc/2442.test.cpp
+++ test/aoj/icpc/2442.test.cpp
@@ -2,12 +2,12 @@
 // verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/2442
 // verification-helper: ERROR 0.0001
 
+#include "src/real-geometry/class/point.hpp"
+#include "src/real-geometry/class/segment.hpp"
+#include "src/real-geometry/position/is-parallel.hpp"
+
 #include <iostream>
 #include <cstdlib>
-
-#include "../../../src/point.hpp"
-#include "../../../src/segment.hpp"
-#include "../../../src/is_parallel.hpp"
 
 using namespace geometry;
 
@@ -17,17 +17,19 @@ void drop() {
 }
 
 int main() {
+  using R = long double;
+
   int n;
   std::cin >> n;
 
   if (n & 1) drop();
 
-  points ps(n);
+  points<R> ps(n);
   for (auto &p : ps) {
     std::cin >> p;
   }
 
-  segments ss;
+  segments<R> ss;
   for (int i = 0; i < n; i++) {
     ss.emplace_back(ps[i], ps[(i + 1) % n]);
   }
@@ -38,10 +40,10 @@ int main() {
       drop();
     }
 
-    if (equals(abs(ss[i].a - ss[i].b), abs(ss[j].a - ss[j].b)) == false) {
+    if (equals(std::abs(ss[i].a - ss[i].b), std::abs(ss[j].a - ss[j].b)) == false) {
       drop();
     }
   }
 
-  std::cout << (ps[0] + ps[(n / 2) % n]) * (1.0 / 2.0) << std::endl;
+  std::cout << (ps[0] + ps[n / 2]) * 0.5 << std::endl;
 }
